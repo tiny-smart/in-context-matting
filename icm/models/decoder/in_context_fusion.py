e72de57bfec9930131650173f01f938db6608773
guohe <649627826@qq.com>
2023-10-04 23:52:18 +0800

    sam_decoder for in context fusion

--- icm/models/decoder/in_context_fusion.py
+++ icm/models/decoder/in_context_fusion.py
@@ -8,6 +8,9 @@ from torch import nn
 from icm.models.attention.attention_sam import TwoWayAttentionBlock, Attention, MLPBlock
 
 from icm.models.decoder.bottleneck_block import BottleneckBlock
+from ptp.ptp_attention_controllers import AttentionStore
+from icm.models.decoder.sam_decoder import MaskDecoder
+
 
 class NoLinearAttention(nn.Module):
     """
@@ -29,7 +32,7 @@ class NoLinearAttention(nn.Module):
         self.v_embedding = nn.Embedding(2, self.internal_dim)
         self.out_proj = nn.Linear(self.internal_dim, embedding_dim)
 
-    def forward(self, q, k, v ):
+    def forward(self, q, k, v):
 
         # Attention
         _, _, _, c_per_head = q.shape
@@ -43,7 +46,7 @@ class NoLinearAttention(nn.Module):
         out = self.out_proj(out)
 
         return out
-    
+
 
 class OneWayAttentionBlock(nn.Module):
     def __init__(
@@ -67,8 +70,9 @@ class OneWayAttentionBlock(nn.Module):
 
     def forward(self, feature_of_reference_image, feature_of_source_image, guidance_on_reference_image):
         h, w = feature_of_reference_image.shape[2:]
-        x = rearrange(feature_of_source_image, "b c h w -> b (h w) c").contiguous()
-        
+        x = rearrange(feature_of_source_image,
+                      "b c h w -> b (h w) c").contiguous()
+
         # k: fg-src, bg-sec+bg_embedding
         # v: fg-src+fg_embedding, bg-sec+bg_embedding
 
@@ -82,7 +86,8 @@ class OneWayAttentionBlock(nn.Module):
         # compute context_k
         embedding_k = torch.matmul((1.0-guidance_on_reference_image.squeeze(
             1).unsqueeze(3).long()), self.context_embed.weight[0].unsqueeze(0))
-        context_k = feature_of_reference_image + embedding_k.permute(0, 3, 1, 2)
+        context_k = feature_of_reference_image + \
+            embedding_k.permute(0, 3, 1, 2)
         context_k = context_k.reshape(
             context_k.shape[0], context_k.shape[1], -1).permute(0, 2, 1)
 
@@ -90,10 +95,11 @@ class OneWayAttentionBlock(nn.Module):
         x = self.norm1(x)
         x = self.mlp(x) + x
         x = self.norm2(x)
-        
+
         x = rearrange(x, "b (h w) c -> b c h w", h=h, w=w).contiguous()
-        
+
         return x
+
 
 class OneWayAttentionBlockFixedValue(nn.Module):
     def __init__(
@@ -118,21 +124,23 @@ class OneWayAttentionBlockFixedValue(nn.Module):
 
     def forward(self, feature_of_reference_image, feature_of_source_image, guidance_on_reference_image):
         h, w = feature_of_reference_image.shape[2:]
-        x = rearrange(feature_of_source_image, "b c h w -> b (h w) c").contiguous()
-        
+        x = rearrange(feature_of_source_image,
+                      "b c h w -> b (h w) c").contiguous()
+
         # k: fg-src, bg-sec+bg_embedding
         # v: fg_embedding_v,bg_embedding_v
 
         # compute context_v
         context_v = self.context_embed(guidance_on_reference_image.squeeze(
-                1).long()).permute(0, 3, 1, 2)
+            1).long()).permute(0, 3, 1, 2)
         context_v = context_v.reshape(
             context_v.shape[0], context_v.shape[1], -1).permute(0, 2, 1)
 
         # compute context_k
         embedding_k = torch.matmul((1.0-guidance_on_reference_image.squeeze(
             1).unsqueeze(3).long()), self.bg_embedding.weight)
-        context_k = feature_of_reference_image + embedding_k.permute(0, 3, 1, 2)
+        context_k = feature_of_reference_image + \
+            embedding_k.permute(0, 3, 1, 2)
         context_k = context_k.reshape(
             context_k.shape[0], context_k.shape[1], -1).permute(0, 2, 1)
 
@@ -140,11 +148,11 @@ class OneWayAttentionBlockFixedValue(nn.Module):
         x = self.norm1(x)
         x = self.mlp(x) + x
         x = self.norm2(x)
-        
+
         x = rearrange(x, "b (h w) c -> b c h w", h=h, w=w).contiguous()
-        
+
         return x
-    
+
 
 class TwoWayAttentionBlock(nn.Module):
     def __init__(
@@ -242,6 +250,7 @@ class OneWayAttentionBlock2(nn.Module):
 
         return queries, keys
 
+
 class InContextTransformer(nn.Module):
     '''
     one implementation of in_context_fusion
@@ -259,17 +268,16 @@ class InContextTransformer(nn.Module):
                  attn_cls=OneWayAttentionBlock,
                  ):
         super().__init__()
-        
-        
+
         self.attn = attn_cls(
             in_dim, n_heads, d_head, mlp_dim_rate) if not use_bottle_neck else OneWayAttentionBlock(bottle_neck_dim, n_heads, d_head, mlp_dim_rate)
-        
+
         self.use_bottle_neck = use_bottle_neck
         if use_bottle_neck:
             self.bottle_neck = BottleneckBlock(in_channels=in_dim,
-                        bottleneck_channels=in_dim // 4,
-                        out_channels=bottle_neck_dim,
-                        norm="GN",)
+                                               bottleneck_channels=in_dim // 4,
+                                               out_channels=bottle_neck_dim,
+                                               norm="GN",)
 
     def forward(self, feature_of_reference_image, feature_of_source_image, guidance_on_reference_image):
         '''
@@ -277,46 +285,188 @@ class InContextTransformer(nn.Module):
         feature_of_source_image: [B, C, H, W]
         guidance_on_reference_image: [B, 1, H_, W_]
         '''
-        
+
         if self.use_bottle_neck:
-            feature_of_reference_image = self.bottle_neck(feature_of_reference_image).detach()
+            feature_of_reference_image = self.bottle_neck(
+                feature_of_reference_image).detach()
             feature_of_source_image = self.bottle_neck(feature_of_source_image)
-            
+
         h, w = feature_of_reference_image.shape[2:]
-        
+
         guidance_on_reference_image = F.interpolate(
             guidance_on_reference_image, size=feature_of_reference_image.shape[2:], mode='nearest')
 
-        
-        feature_of_source_image = self.attn(feature_of_reference_image, feature_of_source_image, guidance_on_reference_image)
+        feature_of_source_image = self.attn(
+            feature_of_reference_image, feature_of_source_image, guidance_on_reference_image)
 
         return feature_of_source_image
 
 
+class InContextTransformer2(nn.Module):
+    '''
+    one implementation of in_context_fusion
+
+    forward(feature_of_reference_image, feature_of_source_image, guidance_on_reference_image)
+    '''
+
+    def __init__(self,
+                 in_dim,
+                 n_heads,
+                 d_head,
+                 mlp_dim_rate,
+                 use_bottle_neck=False,
+                 bottle_neck_dim=512,
+                 mask_dim=3,
+                 ):
+        super().__init__()
+
+        # self.attn = attn_cls(
+        #     in_dim, n_heads, d_head, mlp_dim_rate) if not use_bottle_neck else OneWayAttentionBlock(bottle_neck_dim, n_heads, d_head, mlp_dim_rate)
+        self.attn = MaskDecoder(transformer_dim=in_dim,
+                                num_multimask_outputs=mask_dim,
+                                two_way_transformer_depth=1,
+                                n_head=n_heads,
+                                mlp_dim=int(mlp_dim_rate*in_dim),) if not use_bottle_neck else MaskDecoder(transformer_dim=bottle_neck_dim,
+                                                                                                           num_multimask_outputs=mask_dim,
+                                                                                                           two_way_transformer_depth=1,
+                                                                                                           n_head=n_heads,
+                                                                                                           mlp_dim=int(mlp_dim_rate*bottle_neck_dim),)
+
+        self.use_bottle_neck = use_bottle_neck
+        if use_bottle_neck:
+            self.bottle_neck = BottleneckBlock(in_channels=in_dim,
+                                               bottleneck_channels=in_dim // 4,
+                                               out_channels=bottle_neck_dim,
+                                               norm="GN",)
+
+        self.context_embed = nn.Embedding(
+            2, in_dim) if not use_bottle_neck else nn.Embedding(2, bottle_neck_dim)
+
+    def forward(self, feature_of_reference_image, feature_of_source_image, guidance_on_reference_image):
+        '''
+        feature_of_reference_image: [B, C, H, W]
+        feature_of_source_image: [B, C, H, W]
+        guidance_on_reference_image: [B, 1, H_, W_]
+        '''
+
+        if self.use_bottle_neck:
+            feature_of_reference_image = self.bottle_neck(
+                feature_of_reference_image).detach()
+            feature_of_source_image = self.bottle_neck(feature_of_source_image)
+
+        h, w = feature_of_reference_image.shape[2:]
+
+        guidance_on_reference_image = F.interpolate(
+            guidance_on_reference_image, size=feature_of_reference_image.shape[2:], mode='nearest')
+
+        # use single batch process, concat output
+        feature_of_source_image_ = torch.cat([self.single_batch_process(
+            feature_of_reference_image[i], guidance_on_reference_image[i], feature_of_source_image[i]) for i in range(feature_of_source_image.shape[0])], dim=0)
 
 
-        # if self.context_type == 'maskpooling':
-        #     feature_of_reference_image = self.context_maskpooling(
-        #         feature_of_reference_image, guidance_on_reference_image)
+        # downsample feature_of_source_image /4
+        feature_of_source_image_ = F.interpolate(
+            feature_of_source_image_, size=feature_of_source_image.shape[2:], mode='bilinear')
+        
+        # concatentate feature_of_source_image_ and feature_of_source_image
+        feature_of_source_image = torch.cat(
+            [feature_of_source_image, feature_of_source_image_], dim=1)
+
+        return feature_of_source_image
+
+    def single_batch_process(self, feature_of_reference_image, guidance_on_reference_image, feature_of_source_image):
+        feature_of_reference_image = self.context_maskpooling(
+            feature_of_reference_image, guidance_on_reference_image) # x, c
+
+        # add context_embedding[0] to feature_of_reference_image, concatentate same size context_embedding[1] to feature_of_reference_image
+        feature_of_reference_image = feature_of_reference_image + \
+            self.context_embed.weight[0].unsqueeze(
+                0).repeat(feature_of_reference_image.shape[0], 1)
+        feature_of_reference_image = torch.cat([feature_of_reference_image, self.context_embed.weight[1].unsqueeze(
+            0).repeat(feature_of_reference_image.shape[0], 1)], dim=0).unsqueeze(0) # 2x,c
+
+        # feature_of_source_image_ = rearrange(feature_of_source_image.unsqueeze(0),
+        #                                      "b c h w -> b (h w) c").contiguous() # 1 1024 256
+        feature_of_source_image_ = feature_of_source_image.unsqueeze(0)
+        feature_of_source_image_ = self.attn(
+            feature_of_source_image_, feature_of_reference_image, )
+
+        return feature_of_source_image_
+
+    def context_maskpooling(self, feature, mask):
+        '''
+        get context feature tokens by maskpooling
+        feature: [C, H/d, W/d]
+        mask: [1, H, W]  [0,1]
+        return: [token_num, C]
+        '''
+        mask[mask < 1] = 0
+        mask = -1 * mask
+        kernel_size = mask.shape[1] // feature.shape[1]
+        mask = F.max_pool2d(mask, kernel_size=kernel_size,
+                            stride=kernel_size, padding=0)
+
+        index = mask == -1
+        index = index.reshape(1, -1).squeeze()
+
+        feature = feature.reshape(feature.shape[0], -1).permute(1, 0)
+        feature = feature[index]
+
+        return feature
+# unused
 
 
-        # # unused, move to context_decoder
-        # def context_maskpooling(self, feature, mask):
-        #     '''
-        #     get context feature tokens by maskpooling
-        #     feature: [B, C, H/d, W/d]
-        #     mask: [B, 1, H, W]  [0,1]
-        #     return: [B, token_num, C] token_num = H*W/d^2
-        #     '''
-        #     mask[mask < 1] = 0
-        #     mask = -1 * mask
-        #     kernel_size = mask.shape[2] // feature.shape[2]
-        #     mask = F.max_pool2d(mask, kernel_size=kernel_size,
-        #                         stride=kernel_size, padding=0)
-        #     mask = -1*mask
+def register_attention_control(model, controller, if_softmax=True):
+    def ca_forward(self, place_in_unet):
 
-        #     feature = mask*feature
-        #     feature = feature.reshape(
-        #         feature.shape[0], feature.shape[1], -1).permute(0, 2, 1)
+        def forward(self, q, k, v):
+            # Input projections
+            q = self.q_proj(q)
+            k = self.k_proj(k)
+            v = self.v_proj(v)
 
-        #     return feature
+            # Separate into heads
+            q = self._separate_heads(q, self.num_heads)
+            k = self._separate_heads(k, self.num_heads)
+            v = self._separate_heads(v, self.num_heads)
+
+            # Attention
+            _, _, _, c_per_head = q.shape
+            # B x N_heads x N_tokens x N_tokens
+            attn = q @ k.permute(0, 1, 3, 2)
+            attn = attn / math.sqrt(c_per_head)
+
+            if not if_softmax:
+                controller(attn, True, place_in_unet)
+
+            attn = torch.softmax(attn, dim=-1)
+
+            if if_softmax:
+                controller(attn, True, place_in_unet)
+
+            # Get output
+            out = attn @ v
+            out = self._recombine_heads(out)
+            out = self.out_proj(out)
+
+            return out
+
+        return forward
+
+    def register_recr(net_, count, place_in_unet):
+        if net_.__class__.__name__ == 'Attention':
+            net_.forward = ca_forward(net_, place_in_unet)
+            return count + 1
+        elif hasattr(net_, 'children'):
+            for net__ in net_.children():
+                count = register_recr(net__, count, place_in_unet)
+        return count
+
+    att_count = 0
+    sub_nets = model.named_children()
+
+    for net in sub_nets:
+
+        cross_att_count += register_recr(net, 0, "up")
+
+    controller.num_att_layers = att_count
